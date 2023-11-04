e4064d1229f70572e72c0b058f176164a397e855
Patricia Mae Quitoriano <87472224+patkyu@users.noreply.github.com>
2023-11-04 18:36:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Importing essential libraries and modules
 
-from flask import Flask, render_template, request, Markup
+from flask import Flask, render_template, request
+import Markup
 import numpy as np
 import pandas as pd
 from utils.disease import disease_dic
